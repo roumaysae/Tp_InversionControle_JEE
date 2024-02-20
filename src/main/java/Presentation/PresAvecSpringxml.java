be21c9fff70781f225f811388d1df297f67d7620
Roumaysae <roumaysaeelamrani2002@gmail.com>
2024-02-20 23:47:52 +0100

    Second version

--- src/main/java/Presentation/PresAvecSpringxml.java
+++ src/main/java/Presentation/PresAvecSpringxml.java
@@ -1,5 +1,14 @@
 package Presentation;
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PresAvecSpringxml {
+        public static void main(String[] args) {
+            ApplicationContext springContext=new ClassPathXmlApplicationContext("config.xml");
+            IMetier metier=springContext.getBean(IMetier.class); //by intyerface
+            IMetier metier2=(IMetier) springContext.getBean("metier"); //by name
+            System.out.println(metier.calcul());
+        }
+    }
 
-}
